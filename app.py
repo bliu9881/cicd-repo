9cc02b5a570433b7d9d134eaf36f009da069ad58
brian Liu <bliu9881@gmail.com>
2024-08-01 13:08:04 -0700

    setting up the first lambda stage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from my_pipeline.my_pipeline_stack import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack",
-    env=cdk.Environment(account="779227446268", region="us-west-1")
+    env=cdk.Environment(account="779227446268", region="us-east-1")
 )
 
 app.synth()
